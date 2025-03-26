5022340549bf41773053f757c0063e2715f32e51
Senaka Amarakeerthi <senaka@Senakas-MacBook-Pro.local>
2025-03-26 14:33:44 +1300

    index modifed to display accounts

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // config.php
 $host = 'localhost';
 $user = 'root';
-$pass = 'root';
+$pass = '';
 $dbname = 'vulnapp';
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
@@ -20,6 +20,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($result && $result->num_rows > 0) {
         echo "<div class='alert alert-success text-center'>Welcome, $username!</div>";
+        echo "<div class='text-center'>You entered: Username = $username, Password = $password</div>";
+        echo "<div class='text-center'><strong>Accounts found:</strong><br>";
+        echo "<table class='table table-bordered w-50 mx-auto'><tr><th>ID</th><th>Username</th><th>Password</th></tr>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr><td>{$row['id']}</td><td>{$row['username']}</td><td>{$row['password']}</td></tr>";
+        }
+        echo "</table></div>";
     } else {
         echo "<div class='alert alert-danger text-center'>Invalid credentials</div>";
     }
